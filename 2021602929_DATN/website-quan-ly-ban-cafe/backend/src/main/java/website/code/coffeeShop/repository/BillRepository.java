7b42f8535daeace4b7a9f610b5ca488b23277fa2
NguyenVanTu <tuxoe0403@gmail.com>
2025-05-14 21:23:52 +0700

    commit update file

--- 2021602929_DATN/website-quan-ly-ban-cafe/backend/src/main/java/website/code/coffeeShop/repository/BillRepository.java
+++ 2021602929_DATN/website-quan-ly-ban-cafe/backend/src/main/java/website/code/coffeeShop/repository/BillRepository.java
@@ -20,7 +20,7 @@ public interface BillRepository extends JpaRepository<Bill, Integer> {
 
     @Query("SELECT MONTH(b.createdTime) AS month, SUM(b.totalCost) AS totalRevenue "
             + "FROM Bill b GROUP BY MONTH(b.createdTime)")
-    List<Object[]> findMonthlyRevenue(); //Tra ve 1 mang doi tuong, gom month va totalCost
+    List<Object[]> findMonthlyRevenue(); //Tra ve mot mang doi tuong, gom month va totalCost
 
     @Query("SELECT MONTH(b.createdTime) AS month, COUNT(b.billId) AS totalOrders "
             + "FROM Bill b WHERE b.type = 0 GROUP BY MONTH(b.createdTime)") //COUNT(b.BillId) la tính tong so lan xuat hien cua billId LA PRIMARY KEY
